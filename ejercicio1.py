9fa7237c1a51bc2720032ee1e706cacc96f6f895
margaguerrero <margaguerrerogual@gmail.com>
2023-07-11 18:44:58 +0200

    feat: Prework Python exercises

---
+++ ejercicio1.py
@@ -0,0 +1,26 @@
+"""
+1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor.
+Luego, imprime la variable.
+2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10
+respectivamente. Luego, imprime la suma de a y b .
+3. Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+4. Ejercicio: Calcula el resto de dividir 17 entre 3.
+"""
+
+nombre = "Marga"
+print (nombre)
+
+a = 5
+b = 10
+suma = a + b
+print(suma)
+
+base = 10
+altura = 5
+area = base * altura / 2
+print(area)
+
+a = 17
+b = 3
+resto = a % b
+print(resto)
